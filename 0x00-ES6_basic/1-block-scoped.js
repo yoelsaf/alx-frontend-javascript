20c6c8d65a8a915d31dbb74e5098bf6afc7437ef
yoel <yoelsaf@gmail.com>
2023-04-22 16:27:26 +0300

    bdf

--- 0x00-ES6_basic/1-block-scoped.js
+++
@@ -1,9 +0,0 @@
-export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    return [task, task2];
-  }
-  return [task, task2];
-}
--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const arr = [];
-  for (const idx of array) {
-    const value = idx;
-    arr.push(appendString + value);
-  }
-
-  return arr;
-}
